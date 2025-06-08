0d5ab615cae7218d75e8d9ca1cdcedd95d8fd184
Omid Reza Izadi <info.omidreza@gmail.com>
2025-06-08 14:22:20 +0200

    simplified the settings

--- Editor/Core/EasyCodeGenSettings.cs
+++ Editor/Core/EasyCodeGenSettings.cs
@@ -6,9 +6,7 @@ namespace com.omidizadi.EasyCodeGen
     [CreateAssetMenu(fileName = "EasyCodeGenSettings", menuName = "Easy Code Gen/Settings")]
     public class EasyCodeGenSettings : ScriptableObject
     {
-        [SerializeField] private List<string> _includedAssemblies = new List<string>();
-        
-        public List<string> IncludedAssemblies => _includedAssemblies;
+        private readonly List<string> _includedAssemblies = new List<string>();
 
         private static EasyCodeGenSettings _instance;
 
@@ -19,60 +17,45 @@ namespace com.omidizadi.EasyCodeGen
                 if (_instance == null)
                 {
                     _instance = Resources.Load<EasyCodeGenSettings>("EasyCodeGenSettings");
-                    
-                    // Create settings asset if it doesn't exist
-                    if (_instance == null)
-                    {
-                        _instance = CreateInstance<EasyCodeGenSettings>();
-                        #if UNITY_EDITOR
-                        // Create resources folder if it doesn't exist
-                        if (!System.IO.Directory.Exists("Assets/Resources"))
-                        {
-                            UnityEditor.AssetDatabase.CreateFolder("Assets", "Resources");
-                        }
-                        UnityEditor.AssetDatabase.CreateAsset(_instance, "Assets/Resources/EasyCodeGenSettings.asset");
-                        UnityEditor.AssetDatabase.SaveAssets();
-                        #endif
-                    }
                 }
-                
+
                 return _instance;
             }
         }
-        
+
         public bool IsAssemblyIncluded(string assemblyName)
         {
             return _includedAssemblies.Contains(assemblyName);
         }
-        
+
         public void AddAssembly(string assemblyName)
         {
             if (!_includedAssemblies.Contains(assemblyName))
             {
                 _includedAssemblies.Add(assemblyName);
-                #if UNITY_EDITOR
+#if UNITY_EDITOR
                 UnityEditor.EditorUtility.SetDirty(this);
-                #endif
+#endif
             }
         }
-        
+
         public void RemoveAssembly(string assemblyName)
         {
             if (_includedAssemblies.Contains(assemblyName))
             {
                 _includedAssemblies.Remove(assemblyName);
-                #if UNITY_EDITOR
+#if UNITY_EDITOR
                 UnityEditor.EditorUtility.SetDirty(this);
-                #endif
+#endif
             }
         }
-        
+
         public void ClearAssemblies()
         {
             _includedAssemblies.Clear();
-            #if UNITY_EDITOR
+#if UNITY_EDITOR
             UnityEditor.EditorUtility.SetDirty(this);
-            #endif
+#endif
         }
     }
 }
